4100321dc16f2b2250c12629cecb3fafe5337c66
mailsondias7 <mailsondias7@gmail.com>
2021-01-10 20:33:19 -0300

    Ready to prod

--- backend/src/main/java/com/devsuperior/dsdelivery/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/services/OrderService.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PutMapping;
+
 
 import com.devsuperior.dsdelivery.dto.OrderDTO;
 import com.devsuperior.dsdelivery.dto.ProductDTO;
